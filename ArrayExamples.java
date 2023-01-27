357e20f28ec5f330bf32be2e38c6e840fafe9191
MSI <davidkimgba@gmail.com>
2023-01-26 17:12:20 -0800

    finished array section

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -57,13 +57,15 @@ public class ArrayExamples {
   // 1 element in the array
   static double averageWithoutLowestFixed(double[] arr) {
     if(arr.length < 2) { return 0.0; }
-    double lowest = arr[0];
-    for(double num: arr) {  
-      if(num < lowest) { lowest = num; }
+    int lowestIndex = 0;
+    for(int i = 0; i < arr.length; i++)
+    {
+      if(Double.compare(arr[i], arr[lowestIndex]) < 0){lowestIndex = i;}
     }
     double sum = 0;
-    for(double num: arr) {
-      if(num != lowest) { sum += num; }
+    for(int i = 0; i < arr.length; i++)
+    {
+      if(i != lowestIndex){sum+=arr[i];}
     }
     return sum / (arr.length - 1);
   }
