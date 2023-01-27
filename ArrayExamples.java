17222aa66509a5dcceec515a84069d6e412851bf
MSI <davidkimgba@gmail.com>
2023-01-26 17:02:04 -0800

    commit for desktop

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -35,6 +35,39 @@ public class ArrayExamples {
     return sum / (arr.length - 1);
   }
 
+  static void reverseInPlaceFixed(int[] arr) {
+    int temp;
+    for(int i = 0; i < arr.length/2; i++) {
+      temp = arr[i];
+      arr[i] = arr[arr.length-1-i];
+      arr[arr.length-1-i] = temp;
+    }
+  }
+
+  static int[] reversedFixed(int[] arr) { 
+    int[] newArray = new int[arr.length];
+    for(int i = 0; i < arr.length; i += 1) {
+      newArray[arr.length - i - 1] = arr[i];
+    }
+    return newArray;
+  }
+  
+  // Averages the numbers in the array (takes the mean), but leaves out the
+  // lowest number when calculating. Returns 0 if there are no elements or just
+  // 1 element in the array
+  static double averageWithoutLowestFixed(double[] arr) {
+    if(arr.length < 2) { return 0.0; }
+    double lowest = arr[0];
+    for(double num: arr) {  
+      if(num < lowest) { lowest = num; }
+    }
+    double sum = 0;
+    for(double num: arr) {
+      if(num != lowest) { sum += num; }
+    }
+    return sum / (arr.length - 1);
+  }
+  
 
 }
 
--- ArrayTests.java
+++ ArrayTests.java
@@ -1,5 +1,7 @@
-import static org.junit.Assert.*;
-import org.junit.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 public class ArrayTests {
 	@Test 
@@ -9,10 +11,69 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test
+  public void testReverseInPlaceLongerIntArray()
+  {
+    int[] test = {1, 2, 3, 4, 5};
+    ArrayExamples.reverseInPlace(test);
+    assertArrayEquals(new int[]{5,4,3,2,1}, test);
+  }
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReversedShortArray()
+  {
+    int[] test = {1};
+    ArrayExamples.reversed(test);
+    assertArrayEquals(new int[]{1}, test);
+  }
+
+  @Test
+  public void testAverageWithoutLowest()
+  {
+    double[] test = {};
+    assertEquals((double) 0.0, ArrayExamples.averageWithoutLowest(test),0.00001);
+  }
+
+  @Test
+  public void testAverageWithoutLowestOneNum()
+  {
+    double[] test = {1};
+    assertEquals((double) 0.0, ArrayExamples.averageWithoutLowest(test),0.000001);
+  }
+
+  @Test
+  public void testAverageWithoutLowestAtFirst()
+  {
+    double[] test = {1,3,3,3,3};
+    assertEquals((double) 3, ArrayExamples.averageWithoutLowest(test),0.001);
+  }
+
+  @Test
+  public void testAverageWithoutLowestAtLast()
+  {
+    double[] test = {3,3,3,3,1};
+    assertEquals((double) 3, ArrayExamples.averageWithoutLowest(test),0.001);
+  }
+   
+  @Test
+  public void testAverageWithoutLowestAllSame()
+  {
+    double[] test = {3,3,3,3};
+    assertEquals((double) 3, ArrayExamples.averageWithoutLowest(test),0.001);
+  }
+
+
+  @Test
+  public void testAverageWithoutLowestTwoNegatives()
+  {
+    double[] test = {-3,-3,3,3};
+    assertEquals((double) 1, ArrayExamples.averageWithoutLowest(test),0.001);
+  }
+   
 }
